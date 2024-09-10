8a4889025b599a61295442883db31c0384232d95
rekhaaryal <128026918+rekhaaryal@users.noreply.github.com>
2024-09-10 19:31:36 +0545

    Add TrainService for train management including search, add, update, and persistence with JSON database

--- lib/src/main/java/ticket/booking/services/UserBookingService.java
+++ lib/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,12 +3,14 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.Ticket;
+import ticket.booking.entities.Train;
 import ticket.booking.entities.User;
 import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -63,7 +65,8 @@ public class UserBookingService
     // json --> object(user) = deserialize
     // object(User) --> json = serialize
 
-    public void fetchBookings(){
+    public void fetchBookings()
+    {
         user.printTicket();
     }
 
@@ -85,5 +88,39 @@ public class UserBookingService
         }
 
 
+        }
+
+    public List<Train> getTrains(String source, String destination){
+        try{
+            TrainService trainService = new TrainService();
+            return trainService.searchTrains(source, destination);
+        }catch(IOException ex){
+            return new ArrayList<>();
+        }
+    }
+
+    public List<List<Integer>> fetchSeats(Train train){
+        return train.getSeats();
+    }
+
+    public Boolean bookTrainSeat(Train train, int row, int seat) {
+        try{
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                if (seats.get(row).get(seat) == 0) {
+                    seats.get(row).set(seat, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                    return true; // Booking successful
+                } else {
+                    return false; // Seat is already booked
+                }
+            } else {
+                return false; // Invalid row or seat index
+            }
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
     }
 }
